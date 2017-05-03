f034c4458533724e2696fbc647e53914571568e0
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2017-05-03 15:52:27 +0100

    get_bulk_speed.sh now takes optional date parameter

--- get_bulk_speed.sh
+++ get_bulk_speed.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-url="http://services.swpc.noaa.gov/text/ace-swepam.txt"
+if [ "$1" != '' ]
+then
+  # Get specific date
+  url="ftp://sohoftp.nascom.nasa.gov/sdb/goes/ace/daily/"$1"_ace_swepam_1m.txt"
+else
+  # Get daily
+  url="ftp://sohoftp.nascom.nasa.gov/sdb/goes/ace/daily/ace_swepam_1m.txt"
+fi
 
 wget $url
