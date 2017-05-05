65ae0b8ddb22e3e6acb3b3683161deaaf12ebfe1
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2017-05-05 20:53:49 +0100

    Can plot solar data in raw format

--- plot_solar_data.py
+++ plot_solar_data.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python2
 
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
+
 
 def get_noise_from_file(data_fname, index=8):
     """Loads data file and creates noise from it"""
-    solar_data = np.loadtxt(data_fname, comments=['#', ':'])
-    good_indices = solar_data[:, 8] > -400.0
-    bad_indices = solar_data[:, 8] < -400.0
-    noise_data = solar_data[:, index]
-    max_noise_data = max(noise_data[good_indices])
-    min_noise_data = min(noise_data[good_indices])
-    noise = 2*(noise_data - min_noise_data)\
-        / (max_noise_data - min_noise_data) - 1
-    noise[bad_indices] = 0.0
+    noise_data = parse_data_from_file(fname)
+    good_data = noise_data[noise_data > 0]
+    # average = np.mean(noise_data[good_indices])
+    average = np.median(good_data)
+    max_noise = max(np.fabs(good_data - average))
+    noise = (noise_data - average)/max_noise
+    noise[noise_data < 0] = 0.0
+    percentile = np.percentile(noise, 90)
+    noise = noise/percentile
 
-    plt.plot(noise)
-    plt.show()
+    return noise
 
-get_noise_from_file(sys.argv[1])
+
+def parse_data_from_file(fname, index=8):
+    data = np.loadtxt(fname, comments=['#', ':'])
+    return data[:, index]
+
+
+parser = argparse.ArgumentParser(description='Plots solar data')
+parser.add_argument('--raw', action='store_true',
+                    help='Plot raw data')
+parser.add_argument('fnames', nargs='+',
+                    help='Data files to plot')
+
+args = parser.parse_args()
+
+if args.raw:
+    solar_data = np.concatenate([parse_data_from_file(fname) for fname in args.fnames])
+    good_data = solar_data[np.logical_and(solar_data > 0, solar_data < 800)]
+
+    plt.plot(good_data)
+else:
+    total_noise = np.concatenate([get_noise_from_file(fname) for fname in
+                                  args.fnames])
+    plt.plot(total_noise)
+
+plt.show()
