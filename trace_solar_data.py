ff751547275a98bc59afb59491fb34316ac988f3
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2017-05-16 16:31:08 +0100

    Now using config file + command line args instead of builtins

--- trace_solar_data.py
+++ trace_solar_data.py
@@ -3,6 +3,7 @@
 """Prints art in the style of each line one breath"""
 
 import argparse
+import json
 
 from scipy.spatial import cKDTree
 from numpy import zeros, sin, cos, pi
@@ -11,44 +12,11 @@ import numpy as np
 import cairo
 # import gtk, gobject
 
-ONE = 0
 
-BACK = 1.
-
-DIST_NEAR_INDICES = np.inf
-NUM_NEAR_INDICES = 30
-SHIFT_INDICES = 5
-
-INVERSE_DISTANCE_ENABLED = False
-MAX_ANGLE_NOISE = 1.0
-
-W = 0.9
-PIX_BETWEEN = 11
-
-MAX_POINTS = 2000
-
-START_X = (1.-W)*0.5
-START_Y = (1.-W)*0.5
-
-X_MIN = 0+START_X/2.0
-Y_MIN = 0+START_Y/2.0
-X_MAX = 1-START_X/2.0
-Y_MAX = 1-START_Y
-
-STEP_LENGTH = 0.0
-
-OUTPUT_FOLDER = "output"
-FILENAME = 'solar_lines'
-FILES = 'input_files'
-INPUT_TYPE = ""
-DATA_COLUMN_INDEX = 8
-CALC_GLOBAL_AVERAGE = False
-
-
-def get_near_indices(tree, xy_points, upper_bound, number_of_points):
+def get_near_indices(tree, xy_points, upper_bound, number_of_points, one):
     """Returns lists containing nearest points and distances to those points"""
     dist, data_inds = tree.query(xy_points, k=number_of_points,
-                                 distance_upper_bound=upper_bound, eps=ONE)
+                                 distance_upper_bound=upper_bound, eps=one)
 
     dist = dist.flatten()
     data_inds = data_inds.flatten()
@@ -61,12 +29,12 @@ def get_near_indices(tree, xy_points, upper_bound, number_of_points):
     return dist, data_inds.flatten()
 
 
-def alignment(angle, dist):
+def alignment(angle, dist, use_inverse_distance):
     """Controls how the distance from nearest line works"""
     distance_x = cos(angle)
     distance_y = sin(angle)
 
-    if INVERSE_DISTANCE_ENABLED:
+    if use_inverse_distance:
         # inverse proporional distance scale
         distance_x = np.sum(distance_x/dist)
         distance_y = np.sum(distance_y/dist)
@@ -82,34 +50,28 @@ def alignment(angle, dist):
 
 class Render(object):
     """Renders lines"""
-    def __init__(self, n, ysize):
-        self.size = n
+    def __init__(self, xsize, ysize, background_colour, line_width):
+        self.xsize = xsize
         self.ysize = ysize
-        self.__init_cairo()
+        self.line_width = line_width
+        self.__init_cairo(background_colour)
 
-    def __init_cairo(self):
-
-        sur = cairo.ImageSurface(cairo.FORMAT_ARGB32, self.size, self.ysize)
+    def __init_cairo(self, background_colour):
+        sur = cairo.ImageSurface(cairo.FORMAT_ARGB32, self.xsize, self.ysize)
         ctx = cairo.Context(sur)
-        ctx.scale(self.size, self.ysize)
-        ctx.set_source_rgb(BACK, BACK, BACK)
+        ctx.scale(self.xsize, self.ysize)
+        ctx.set_source_rgb(background_colour,
+                           background_colour, background_colour)
         ctx.rectangle(0, 0, 1, 1)
         ctx.fill()
 
         self.sur = sur
         self.ctx = ctx
 
-    def line(self, xy_points, colour=""):
+    def line(self, xy_points, colour=(0, 0, 0)):
         """Add a line joining the points in xy_points"""
-        red, green, blue = 0, 0, 0
-        if colour == "red":
-            red = 255
-        if colour == "blue":
-            blue = 255
-        if colour == "green":
-            green = 255
-        self.ctx.set_source_rgba(red, green, blue, 0.6)
-        self.ctx.set_line_width(ONE*3.)
+        self.ctx.set_source_rgba(colour[0], colour[1], colour[2], 0.6)
+        self.ctx.set_line_width(self.line_width)
 
         self.ctx.move_to(xy_points[0, 0], xy_points[0, 1])
         for (x_pt, y_pt) in xy_points[1:]:
@@ -117,46 +79,51 @@ class Render(object):
         self.ctx.stroke()
 
 
-def generate_line(start_x, start_y, start_angle, xy_old, angles_old, noise,
-                  max_points):
+def generate_line(config, xy_old, angles_old, noise, is_first):
     """Generates a line following the old line and introducing noise"""
 
-    if xy_old != []:
+    max_points = len(noise)
+
+    if not is_first:
         tree = cKDTree(xy_old)
     xy_new = zeros((max_points, 2), 'float')
     angles = zeros(max_points, 'float')
 
-    xy_point = np.array([[start_x, start_y]])
+    xy_point = np.array([[config["xy_start"][0], config["xy_start"][1]]])
 
     xy_new[0, :] = xy_point
     angles[0] = 0.5*pi
-    angle = 0.5*pi
 
     for i in xrange(1, max_points):
-        angle = 0.5*pi
-        if xy_old != []:
+        if not is_first:
             dist, inds = get_near_indices(tree, xy_point,
-                                          DIST_NEAR_INDICES, NUM_NEAR_INDICES)
+                                          np.inf, config["num_near_indices"],
+                                          config["one"])
 
             # dist[dist<ONE] = ONE
-            dist = dist[SHIFT_INDICES:]
-            inds = inds[SHIFT_INDICES:]
+            dist = dist[config["num_shift_indices"]:]
+            inds = inds[config["num_shift_indices"]:]
 
-            distance_x, distance_y = alignment(angles_old[inds], dist)
+            distance_x, distance_y = alignment(angles_old[inds], dist,
+                                               config["use_global_average"])
 
-            angle = np.arctan2(distance_y, distance_x)
-        if i < len(noise):
-            angle += noise[i]
+            angles[i] += np.arctan2(distance_y, distance_x)
+        else:
+            angles[i] = 0.5*pi
 
-        xy_next_point = xy_point \
-            + np.array([[cos(angle), sin(angle)]])*STEP_LENGTH
+        angles[i] += noise[i]
+
+        xy_next_point = xy_point\
+            + np.array([[cos(angles[i]), sin(angles[i])]])\
+            * config["step_length"]
         xy_new[i, :] = xy_next_point
-        angles[i] = angle
 
         xy_point = xy_next_point
 
-        if xy_point[0, 0] > X_MAX or xy_point[0, 0] < X_MIN or\
-           xy_point[0, 1] > Y_MAX or xy_point[0, 1] < Y_MIN:
+        if xy_point[0, 0] > config["xy_max"][0] or\
+           xy_point[0, 0] < config["xy_min"][0] or\
+           xy_point[0, 1] > config["xy_max"][1] or\
+           xy_point[0, 1] < config["xy_min"][1]:
             xy_new = xy_new[:i, :]
             angles = angles[:i]
             break
@@ -164,174 +131,136 @@ def generate_line(start_x, start_y, start_angle, xy_old, angles_old, noise,
     return xy_new, angles
 
 
-def draw_image():
-    """Draws each line one breath image"""
-    with open(FILES, 'r') as data_filenames_fp:
-        data_filenames = data_filenames_fp.read().split()
+def split_uneven_array(array, N):
+    """Split uneven array into """
+    i = 1
+    while i*N < len(array):
+        yield array[(i-1)*N:i*N]
+        i += 1
 
-    if CALC_GLOBAL_AVERAGE:
-        average_noise = calculate_average_noise()
+    yield array[(i-1)*N:]
+
+
+def calculate_noise(noise_data, max_angle_noise):
+    """Returns normalised and centred noise data"""
+    good_data = noise_data[noise_data >= 0]
+    average = np.median(good_data)
+    noise = np.zeros_like(noise_data)
+    if (good_data != 0.0).any():
+        noise = (noise_data - average)/max(np.fabs(good_data - average))
+    noise[noise_data < 0] = 0.0
+    noise *= max_angle_noise
+
+    return noise
+
+
+def get_noise_from_file(config):
+    """Loads noise from given file"""
+    noise_data = np.genfromtxt(config["noise_file"])
+    noise_data = noise_data[:-(len(noise_data) % config["points_per_line"])]
+
+    if config["use_global_average"]:
+        noise = calculate_noise(noise_data, config["max_angle_noise"])
     else:
-        average_noise = np.inf
+        noise = np.concatenate(
+            [calculate_noise(array, config["max_angle_noise"]) for array in
+             np.split(noise_data, config["points_per_line"])])
 
-    num_lines = len(data_filenames)
-    line_sep = W/num_lines
-    size = int((num_lines-1)*PIX_BETWEEN/W)
+    return noise
 
-    test_noise = get_noise_from_file(data_filenames[0], INPUT_TYPE,
-                                     average=average_noise)
-    global START_X, START_Y, X_MAX, Y_MAX, X_MIN, Y_MIN
-    ysize = len(test_noise) + (START_X + (1 - X_MAX))*size
 
-    START_Y = START_X*float(size)/float(ysize)
-    Y_MAX = 1-START_Y
-    Y_MIN = START_Y/2.0
+def draw_image(config):
+    """Draws each line one breath image"""
 
-    global ONE, STEP_LENGTH
-    ONE = 1./size
-    STEP_LENGTH = 1./ysize
+    noise = get_noise_from_file(config)
 
-    render = Render(size, ysize=int(ysize))
+    num_lines = len(noise)/config["points_per_line"]
+    xsize = int((num_lines-1)*config["pixels_per_line"]/config["inner_width"])
+    ysize = int(config["points_per_line"]/config["inner_width"])
+    config["one"] = 1./xsize
+    config["step_length"] = 1./ysize
+    render = Render(xsize, ysize, 0.9, 3*config["one"])
 
-    noise = get_noise_from_file(data_filenames[0], INPUT_TYPE,
-                                average=average_noise)
-    max_points = len(noise)
-    noise = np.zeros_like(noise)
-    line_points, angles = generate_line(START_X, START_Y,
-                                        pi*0.5, [], [], noise, max_points)
+    config["xy_start"] = [(1-config["inner_width"])/2.0,
+                          (1-config["inner_width"])/2.0]
+    config["xy_min"] = [config["xy_start"][0]/2.0,
+                        config["xy_start"][1]/2.0]
+    config["xy_max"] = [1-config["xy_start"][0]/2.0,
+                        1-config["xy_start"][1]/2.0]
 
-    render.line(line_points, "red")
+    print config
 
-    for line_num in range(1, num_lines):
-        print(line_num, num_lines)
-        noise = get_noise_from_file(data_filenames[line_num], INPUT_TYPE,
-                                    average=average_noise)
-        line_points, angles = generate_line(START_X+line_num*line_sep, START_Y,
-                                            pi*0.5, line_points, angles, noise,
-                                            max_points)
-        if line_num % 27 == 0:
-            colour = "red"
+    line_sep = config["inner_width"]/num_lines
+    xy_points = np.zeros((config["points_per_line"], 2))
+    angles = np.zeros(config["points_per_line"])
+    for line_num, noise_array in enumerate(
+            np.split(noise, num_lines)):
+        if line_num % 100 == 0:
+            print line_num+1, "/", num_lines
+            if not config["print_final"]:
+                render.sur.write_to_png('{:s}_{:d}.png'.format(
+                    config["output"], line_num))
+
+        xy_points, angles = generate_line(config, xy_points, angles,
+                                          noise_array, line_num == 0)
+
+        config["xy_start"][0] += line_sep
+
+        colour = (0, 0, 0)
+        # if line_num % 24 == 0:
+            # colour = (255, 0, 0)
+        # elif line_num == 24*7 + 16:
+            # colour = (0, 0, 255)
+
+        if colour:
+            render.line(xy_points, colour)
         else:
-            colour = ""
-        render.line(line_points, colour)
+            render.line(xy_points)
 
-        # if line_num % 100 == 0:
-            # render.sur.write_to_png('{:s}_{:d}.png'.format(FILENAME, line_num))
+    render.sur.write_to_png('{:s}.png'.format(config["output"]))
 
-    render.sur.write_to_png('{:s}/{:s}_final.png'
-                            .format(OUTPUT_FOLDER, FILENAME))
+
+def parse_arguments():
+    """Parse... arguments"""
+    with open("config.json", 'r') as config_file:
+        config = json.load(config_file)
+
+    parser = argparse.ArgumentParser(description='Renders solar data as each\
+                                     line one breath')
+
+    parser.add_argument('--num_near_indices', type=int, default=30,
+                        help='Number of nearest neighbours')
+    parser.add_argument('--num_shift_indices', type=int, default=5,
+                        help='Number of nearest neighbours to ignore')
+    parser.add_argument('--max_angle_noise', type=float, default=1.0,
+                        help='Maximum angle in degrees')
+    parser.add_argument('--use_inverse_distance', action='store_true',
+                        help='Switch on inverse distance as opposed to linear')
+    parser.add_argument('--use_global_average', action='store_true',
+                        help='Calc average over all noise instead of per line')
+    parser.add_argument('--output', default='solar_breath')
+    parser.add_argument('--noise_file', required=True)
+    parser.add_argument('--points_per_line', required=True, type=int)
+    parser.add_argument('--print_final', action='store_true')
+
+    args = parser.parse_args()
+    config["num_near_indices"] = args.num_near_indices
+    config["num_shift_indices"] = args.num_shift_indices
+    config["use_inverse_distance"] = args.use_inverse_distance
+    config["use_global_average"] = args.use_global_average
+    config["max_angle_noise"] = args.max_angle_noise
+    config["output"] = args.output
+    config["points_per_line"] = args.points_per_line
+    config["noise_file"] = args.noise_file
+    config["print_final"] = args.print_final
+
+    return config
 
 
 def main():
     """Main"""
-    parser = argparse.ArgumentParser(description='Renders solar data as each\
-                                     line one breath')
-
-    global INVERSE_DISTANCE_ENABLED, NUM_NEAR_INDICES,\
-        SHIFT_INDICES, MAX_ANGLE_NOISE, FILENAME
-
-    parser.add_argument('--num_near_indices', type=int,
-                        default=NUM_NEAR_INDICES, help='Number of nearest\
-                        neighbours')
-    parser.add_argument('--inverse_distance_enabled', action='store_true',
-                        help='Switch on inverse distance as\
-                        opposed to linear')
-    parser.add_argument('--global_average', action='store_true', help='Calc\
-                        average globally instead of per day')
-    parser.add_argument('--shift_indices', type=int, default=SHIFT_INDICES,
-                        help='Number of nearest neighbours to\
-                        ignore')
-    parser.add_argument('--max_angle_noise', type=float,
-                        default=MAX_ANGLE_NOISE, help='Maximum angle in\
-                        degrees')
-    parser.add_argument('--index', type=int, required=True, help='Index of column of data to\
-                        trace')
-    parser.add_argument('--input_type', help='Type of input', required=True,
-                        choices=["ace", "goes"])
-
-    args = parser.parse_args()
-
-    INVERSE_DISTANCE_ENABLED = args.inverse_distance_enabled
-    print "INVERSE_DISTANCE_ENABLED:" + str(INVERSE_DISTANCE_ENABLED)
-
-    if "num_near_indices" in vars(args):
-        NUM_NEAR_INDICES = args.num_near_indices
-        print "Setting NUM_NEAR_INDICES to " + str(NUM_NEAR_INDICES)
-
-    if "shift_indices" in vars(args):
-        SHIFT_INDICES = args.shift_indices
-        print "Setting SHIFT_INDICES to " + str(SHIFT_INDICES)
-
-    if "max_angle_noise" in vars(args):
-        MAX_ANGLE_NOISE = args.max_angle_noise/180.0*pi
-        print "Setting MAX_ANGLE_NOISE to " + str(args.max_angle_noise)
-
-    global INPUT_TYPE
-    INPUT_TYPE = args.input_type
-
-    global DATA_COLUMN_INDEX
-    DATA_COLUMN_INDEX = args.index
-
-    global CALC_GLOBAL_AVERAGE
-    CALC_GLOBAL_AVERAGE = args.global_average
-    print "CALC_GLOBAL_AVERAGE:" + str(CALC_GLOBAL_AVERAGE)
-
-    global FILENAME
-    if INVERSE_DISTANCE_ENABLED:
-        FILENAME = "inverse_" + FILENAME
-    if CALC_GLOBAL_AVERAGE:
-        FILENAME = "glob_av_" + FILENAME
-
-    FILENAME = "sh"+str(SHIFT_INDICES) + "_nn"+str(NUM_NEAR_INDICES) +\
-        "_maxang"+str(args.max_angle_noise) + "_" + FILENAME
-
-    draw_image()
-
-
-def calculate_average_noise():
-    with open(FILES, 'r') as data_filenames_fp:
-        data_filenames = data_filenames_fp.read().split()
-
-    index = DATA_COLUMN_INDEX
-    total_noise_data = np.array([])
-    for data_fname in data_filenames:
-        solar_data = np.loadtxt(data_fname, comments=['#', ':'])
-        total_noise_data = np.concatenate([total_noise_data,
-                                           solar_data[:, index].flatten()])
-
-    return np.median(total_noise_data[total_noise_data > 0])
-
-
-def parse_data_from_file(fname, index, data_type):
-    if data_type == "goes":
-        data = np.genfromtxt(fname, skip_header=140, delimiter=',')
-    elif data_type == "ace":
-        data = np.genfromtxt(fname, comments=['#', ':'])
-    return data[:, index]
-
-
-def get_noise_from_file(data_fname, data_type, average=np.inf):
-    noise_data = parse_data_from_file(data_fname, DATA_COLUMN_INDEX, data_type)
-    noise_data = noise_data[:MAX_POINTS]
-
-    good_data = noise_data[noise_data >= 0]
-
-    # Calculate average
-    # average = np.mean(noise_data[good_indices])
-    if not CALC_GLOBAL_AVERAGE:
-        average = np.median(good_data)
-
-    # Make average sit at 0 & normalise
-    noise = np.zeros_like(noise_data)
-    if (good_data != 0.0).any():
-        noise = (noise_data - average)/max(np.fabs(good_data -
-                                                   average))
-    # Get rid of bad points
-    noise[noise_data < 0] = 0.0
-
-    noise *= MAX_ANGLE_NOISE
-
-    return noise
+    config = parse_arguments()
+    draw_image(config)
 
 
 if __name__ == '__main__':
