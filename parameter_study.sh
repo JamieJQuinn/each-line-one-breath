334bff00f617d070dd7b66dbb0e708a69f932101
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2017-05-16 16:28:42 +0100

    Redo parameter study file

--- parameter_study.sh
+++ parameter_study.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-for inverse_enabled in '' '--inverse_distance_enabled'; do
-  for num_nearest_neighbours in 5 10 20 30; do
-    for shift_indices in 1 3 5; do
-      for max_angle in 0.1 0.5 1.0 2.0 5.0; do
-        echo "./trace_solar_data.py $inverse_enabled\
-        --num_near_indices=$num_nearest_neighbours\
-        --shift_indices=$shift_indices --max_angle_noise=$max_angle"
-      done
-    done
-  done
+for max_angle in 0.02 0.0175 0.015 0.0125; do
+  echo "./trace_solar_data.py --noise_file noise_data --points_per_line=12671\
+  --max_angle_noise=$max_angle --output=$max_angle --print_final"
 done
